a388ede82dc6283399acc92f2579491785a56dec
jotavgalves <jvgacontato@gmail.com>
2026-08-17 13:30:22 -0300

    Bump PWA shell for native mobile rebuild

--- apps/web/public/sw.js
+++ apps/web/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gtrz-mail-shell-v6';
+const CACHE = 'gtrz-mail-shell-v7';
 const SHELL = ['/', '/manifest.webmanifest', '/favicon.svg', '/brand/gtrz-symbol.svg', '/brand/gtrz-lockup.svg'];
 
 function isCacheableRequest(request, url) {
