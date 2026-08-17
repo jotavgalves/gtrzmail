5a606ea03dd2fbff00b24ff727be4814c686a6a7
jotavgalves <jvgacontato@gmail.com>
2026-08-17 12:10:39 -0300

    Add VAPID secret bindings

--- worker/src/env.ts
+++ worker/src/env.ts
@@ -2,6 +2,8 @@ export type SecretBindings = {
   MASTER_KEY_B64: string;
   RESEND_API_KEY: string;
   RESEND_WEBHOOK_SECRET: string;
+  VAPID_PUBLIC_KEY: string;
+  VAPID_PRIVATE_KEY: string;
 };
 
 export type AppEnv = Env & SecretBindings;
