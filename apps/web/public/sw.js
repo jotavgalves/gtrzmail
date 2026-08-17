43eef65c3b76be135fa51b6788ed50a47ee7b214
jotavgalves <jvgacontato@gmail.com>
2026-08-17 14:03:39 -0300

    Refresh mobile composer shell cache

--- apps/web/public/sw.js
+++ apps/web/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gtrz-mail-shell-v9';
+const CACHE = 'gtrz-mail-shell-v10';
 const SHELL = ['/', '/manifest.webmanifest', '/favicon.svg', '/brand/gtrz-symbol.svg', '/brand/gtrz-lockup.svg'];
 
 function isCacheableRequest(request, url) {
