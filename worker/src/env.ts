c57881f2bd25bbde7e7bf1eec4fcfcc59048be27
jotavgalves <jvgacontato@gmail.com>
2026-08-18 08:33:52 -0300

    Add Turnstile bindings

--- worker/src/env.ts
+++ worker/src/env.ts
@@ -6,6 +6,8 @@ export type SecretBindings = {
   RESEND_WEBHOOK_SECRET: string;
   VAPID_PUBLIC_KEY: string;
   VAPID_PRIVATE_KEY: string;
+  TURNSTILE_SITE_KEY?: string;
+  TURNSTILE_SECRET_KEY?: string;
 };
 
 export type AppEnv = Env & SecretBindings;
