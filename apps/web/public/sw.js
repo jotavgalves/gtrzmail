ee9cd05a51a63bbd3d4c7ae17bb2cbf535872e9a
jotavgalves <jvgacontato@gmail.com>
2026-08-18 08:38:34 -0300

    Bump PWA shell for login protection UI

--- apps/web/public/sw.js
+++ apps/web/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gtrz-mail-shell-v11';
+const CACHE = 'gtrz-mail-shell-v12';
 const SHELL = ['/', '/manifest.webmanifest', '/favicon.svg', '/brand/gtrz-symbol.svg', '/brand/gtrz-lockup.svg'];
 
 function isCacheableRequest(request, url) {
